7c2c53d9e831e04ab8eaa39cdcd0a2108181174a
Jon Robson <jdlrobson@gmail.com>
2011-07-21 17:11:17 +0100

    give marker a size
    otherwise they look bloated

--- src/travelblog.js
+++ src/travelblog.js
@@ -238,7 +238,10 @@ function printMap(url) {
 				new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
 				map.getProjectionObject() // to Spherical Mercator Projection
 			);
-		var marker = new OpenLayers.Marker(lonLat);
+		var size = new OpenLayers.Size(8, 8);
+		var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+		var icon = new OpenLayers.Icon("/marker.png", size, offset);
+		var marker = new OpenLayers.Marker(lonLat, icon);
 		if(title) {
 			marker.events.register('dblclick', marker, function(ev) { 
 				printUrl("/" + title);
